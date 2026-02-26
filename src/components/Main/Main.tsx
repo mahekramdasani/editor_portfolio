eb3ed4b863e374b64fbf3dd3e905b018f2f37a4a
Mahek Ramdasani <mahekramdasani@gmail.com>
2026-02-26 13:42:17 +0530

    removed unnecessary imports

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,19 +4,6 @@ import { Hero } from "../Hero/Hero";
 import { About } from "../About/About";
 import { Contact } from "../Contact/Contact";
 import { Project } from "../Project/Project";
-import python from "../../assets/python.svg"
-import java from "../../assets/java.svg"
-import wordpress from "../../assets/wordpress.svg";
-import shopify from "../../assets/shopify.svg";
-import htmlIcon from "../../assets/html-icon.svg";
-import cssIcon from "../../assets/css-icon.svg";
-import sassIcon from "../../assets/sass-icon.svg";
-import jsIcon from "../../assets/js-icon.svg";
-// import nodeIcon from "../../assets/node-icon.svg";
-import reactIcon from "../../assets/react-icon.svg";
-import typescriptIcon from "../../assets/typescript-icon.svg";
-import vueIcon from "../../assets/vue-icon.svg";
-import boostrapIcon from "../../assets/bootstrap-icon.svg";
 import capcutIcon from "../../assets/Capcut-icon.svg.png";
 import inshotIcon from "../../assets/images.jpg"
 import lrIcon from "../../assets/lr.png"
